b7667cf2a8744f82bcf9c929ce78884d9376adee
megharana <megharanait95@gmail.com>
2018-08-03 01:28:22 +0530

    Deleted unnecessary files

--- DjangoGithubDataFetcher/settings.py
+++ DjangoGithubDataFetcher/settings.py
@@ -50,8 +50,11 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+	'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	
+	'social_django.middleware.SocialAuthExceptionMiddleware',
 ]
 
 CORS_ORIGIN_ALLOW_ALL=True
